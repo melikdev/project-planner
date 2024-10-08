62beb0225f75c8b37c035788bf362b1068956e58
Melik Unesi <melikdev@gmail.com>
2024-10-09 02:54:46 +0300

    added progress bar for single project component

--- src/app/(main)/components/ProjectList.tsx
+++ src/app/(main)/components/ProjectList.tsx
@@ -2,7 +2,7 @@ import SingleProject from "./SingleProject";
 
 const ProjectList = () => {
   return (
-    <div className="gap-10 pb-10 grid lg:grid-cols-3 p-0 md:grid-cols-2 content-center ">
+    <div className="gap-4 pb-10 grid lg:grid-cols-3 p-0 md:grid-cols-2 content-center ">
       <SingleProject />
       <SingleProject />
       <SingleProject />
